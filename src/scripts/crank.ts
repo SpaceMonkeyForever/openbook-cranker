225401bbf862bf7c99eba1b9efb5e0070c607dca
dhruvsol <dhruvrajsinghsolanki161@gmail.com>
2023-02-05 22:34:36 +0530

    feat: added top market support

--- src/scripts/crank.ts
+++ src/scripts/crank.ts
@@ -10,7 +10,8 @@ import {
   Connection,
   PublicKey,
   Transaction,
-  ComputeBudgetProgram, BlockhashWithExpiryBlockHeight,
+  ComputeBudgetProgram,
+  BlockhashWithExpiryBlockHeight,
 } from '@solana/web3.js';
 import { getMultipleAccounts, sleep } from '../utils/utils';
 import BN from 'bn.js';
@@ -21,6 +22,7 @@ import {
 } from '@project-serum/serum';
 import { Token, TOKEN_PROGRAM_ID } from '@solana/spl-token';
 import { Logger } from 'tslog';
+import axios from 'axios';
 
 const {
   ENDPOINT_URL,
@@ -31,22 +33,23 @@ const {
   MAX_UNIQUE_ACCOUNTS,
   CONSUME_EVENTS_LIMIT,
   CLUSTER,
-  HIGH_FEE_MARKETS,      // markets to apply a priority fee for
-  PRIORITY_QUEUE_LIMIT,  // queue length at which to apply the priority fee
-  DEFAULT_CU_PRICE,     // extra microlamports per cu for any market
-  PRIORITY_CU_PRICE,     // extra microlamports per cu for high fee markets
-  PRIORITY_CU_LIMIT,     // compute limit
+  HIGH_FEE_MARKETS, // markets to apply a priority fee for
+  PRIORITY_QUEUE_LIMIT, // queue length at which to apply the priority fee
+  DEFAULT_CU_PRICE, // extra microlamports per cu for any market
+  PRIORITY_CU_PRICE, // extra microlamports per cu for high fee markets
+  PRIORITY_CU_LIMIT, // compute limit
+  TOP_MARKET,
 } = process.env;
 
 const cluster = CLUSTER || 'mainnet';
 const interval = INTERVAL || 1000;
 const maxUniqueAccounts = parseInt(MAX_UNIQUE_ACCOUNTS || '10');
 const consumeEventsLimit = new BN(CONSUME_EVENTS_LIMIT || '30');
-const priorityMarkets: number[] = JSON.parse(HIGH_FEE_MARKETS || "[0,1]");
-const priorityQueueLimit = parseInt(PRIORITY_QUEUE_LIMIT || "100");
-const defaultPriorityCuPrice = parseInt(DEFAULT_CU_PRICE || "0");
-const priorityCuPrice = parseInt(PRIORITY_CU_PRICE || "100000");
-const CuLimit = parseInt(PRIORITY_CU_LIMIT || "50000");
+const priorityMarkets: number[] = JSON.parse(HIGH_FEE_MARKETS || '[0,1]');
+const priorityQueueLimit = parseInt(PRIORITY_QUEUE_LIMIT || '100');
+const defaultPriorityCuPrice = parseInt(DEFAULT_CU_PRICE || '0');
+const priorityCuPrice = parseInt(PRIORITY_CU_PRICE || '100000');
+const CuLimit = parseInt(PRIORITY_CU_LIMIT || '50000');
 const serumProgramId = new PublicKey(
   PROGRAM_ID || cluster == 'mainnet'
     ? 'srmqPvymJeFKQ4zGQed1GFppgkRHL9kaELCbyksJtPX'
@@ -54,48 +57,66 @@ const serumProgramId = new PublicKey(
 );
 const walletFile = WALLET_PATH || os.homedir() + '/.config/solana/devnet.json';
 const payer = Keypair.fromSecretKey(
-  Uint8Array.from(
-    JSON.parse(
-      KEYPAIR || fs.readFileSync(walletFile, 'utf-8'),
-    ),
-  ),
+  Uint8Array.from(JSON.parse(KEYPAIR || fs.readFileSync(walletFile, 'utf-8'))),
 );
 
-const log: Logger = new Logger({name: "openbook-cranker", displayFunctionName: false, displayFilePath: "hidden", minLevel: "info"});
+const log: Logger = new Logger({
+  name: 'openbook-cranker',
+  displayFunctionName: false,
+  displayFilePath: 'hidden',
+  minLevel: 'info',
+});
 
 log.info(payer.publicKey.toString());
 
 const connection = new Connection(ENDPOINT_URL!, 'processed' as Commitment);
 
 let recentBlockhash: BlockhashWithExpiryBlockHeight;
-connection.getLatestBlockhash(
-  "finalized"
-).then((blockhash) => {recentBlockhash = blockhash;});
+connection.getLatestBlockhash('finalized').then((blockhash) => {
+  recentBlockhash = blockhash;
+});
 
 setInterval(
   async () =>
-    (recentBlockhash = await connection.getLatestBlockhash(
-      "finalized"
-    )),
+    (recentBlockhash = await connection.getLatestBlockhash('finalized')),
   // need to be frequent else we will be sending same TX signature often
   1000,
 );
 
-
 async function run() {
-  const spotMarkets = await Promise.all(
-    markets[cluster].map((m) => {
-      return Market.load(
-        connection,
-        new PublicKey(m.address),
-        {
-          skipPreflight: true,
-          commitment: 'processed' as Commitment,
-        },
-        serumProgramId,
-      );
-    }),
-  );
+  let spotMarkets;
+  if (TOP_MARKET === 'false') {
+    spotMarkets = await Promise.all(
+      markets[cluster].map((m) => {
+        return Market.load(
+          connection,
+          new PublicKey(m.address),
+          {
+            skipPreflight: true,
+            commitment: 'processed' as Commitment,
+          },
+          serumProgramId,
+        );
+      }),
+    );
+  } else {
+    const { data } = await axios.get(
+      'https://openserum.io/api/serum/markets.json?min24hVolume=100000',
+    );
+    spotMarkets = await Promise.all(
+      data.map((m) => {
+        return Market.load(
+          connection,
+          new PublicKey(m.address),
+          {
+            skipPreflight: true,
+            commitment: 'processed' as Commitment,
+          },
+          serumProgramId,
+        );
+      }),
+    );
+  }
 
   const quoteToken = new Token(
     connection,
@@ -171,25 +192,31 @@ async function run() {
         transaction.add(
           ComputeBudgetProgram.setComputeUnitLimit({
             units: CuLimit,
-          })
+          }),
         );
         if (events.length > priorityQueueLimit) {
           const isHighFeeMarket = priorityMarkets.includes(i);
           transaction.add(
             ComputeBudgetProgram.setComputeUnitPrice({
-              microLamports: isHighFeeMarket ? priorityCuPrice : defaultPriorityCuPrice,
-            })
+              microLamports: isHighFeeMarket
+                ? priorityCuPrice
+                : defaultPriorityCuPrice,
+            }),
           );
         }
         transaction.add(instr);
 
-        log.info(`market ${i} sending consume events for ${events.length} events`);
+        log.info(
+          `market ${i} sending consume events for ${events.length} events`,
+        );
 
         transaction.sign(payer);
-        connection.sendRawTransaction(transaction.serialize(), {
-          skipPreflight: true,
-          maxRetries: 2,
-        }).then(x => log.info(`Cranked market ${i}: ${x}`));
+        connection
+          .sendRawTransaction(transaction.serialize(), {
+            skipPreflight: true,
+            maxRetries: 2,
+          })
+          .then((x) => log.info(`Cranked market ${i}: ${x}`));
       }
       await sleep(interval);
     } catch (e) {
